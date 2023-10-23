65cbe3175be71ada6f3881a29e9bc2f6a97eb463
daniel-nolan85 <danielnolan85@yahoo.com>
2023-10-23 08:54:25 -0700

    -Testing badge API

--- public/service-worker-custom.js
+++ public/service-worker-custom.js
@@ -1,7 +1,17 @@
+let currentBadgeCount = 0;
+
 self.addEventListener('push', (e) => {
   const data = e.data.json();
+  console.log('push notification => ', data);
+  console.log('currentBadgeCount => ', currentBadgeCount);
   self.registration.showNotification(data.title, {
     body: data.body,
     icon: data.icon,
   });
+
+  if (navigator.setAppBadge) {
+    const newBadgeCount = currentBadgeCount + 1;
+    navigator.setAppBadge(newBadgeCount);
+    currentBadgeCount = newBadgeCount;
+  }
 });
